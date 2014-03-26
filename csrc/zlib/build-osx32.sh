8fbed171056454d246bdeb071179c6c9159d3e69
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:10:05 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/zlib/build-osx32.sh
+++ csrc/zlib/build-osx32.sh
@@ -1,1 +1,1 @@
-gcc -arch i386 -O2 *.c -shared -o ../../bin/osx32/libz.dylib -I.
+gcc -arch i386 -O2 *.c -shared -install_name @loader_path/libz.dylib -o ../../bin/osx32/libz.dylib -I.
--- csrc/zlib/build-osx64.sh
+++ csrc/zlib/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -arch x86_64 -O2 *.c -shared -o ../../bin/osx64/libz.dylib -I.
+gcc -arch x86_64 -O2 *.c -shared -install_name @loader_path/libz.dylib -o ../../bin/osx64/libz.dylib -I.
